aac8bd7ebd2849fbee958d292002ce6dfc351acc
nagaohiroki <testplayers0226@gmail.com>
2019-02-14 17:00:48 +0900

    add project

---
+++ UnityProject/Assets/Scripts/MaterialTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class MaterialTest : MonoBehaviour
+{
+	[SerializeField]
+	GameObject mGameObjectPrefab = null;
+	GameObject mGameObject;
+	void Update()
+	{
+		if(mGameObject == null)
+		{
+			if(Input.GetKeyDown(KeyCode.A))
+			{
+				mGameObject = Instantiate(mGameObjectPrefab);
+			}
+			return;
+		}
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			Destroy(mGameObject);
+		}
+	}
+}
